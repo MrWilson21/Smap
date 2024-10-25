49950323c2d7d27b5740fe2750dd26964af9b40d
Alexander Haines <alex.haines@artlist.io>
2024-10-25 11:43:50 +0100

    text

--- smap/pages/_app.tsx
+++ smap/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+
+import { Bellota } from 'next/font/google';
+const inter = Bellota({ subsets: ['latin'], weight: "300" });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- smap/pages/homepage.tsx
+++ smap/pages/homepage.tsx
@@ -1,3 +1,4 @@
+import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import Image from 'next/image';
 import Link from 'next/link';
@@ -6,6 +7,7 @@ export default function Component() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-teal-500 gap-4">
       <Image src="/smap.png" alt="smap" width={300} height={400} />
+      <Typography variant="h5" fontFamily="fantasy">Always follow your nose!</Typography>
       <Link href="/map">
         <Button className="px-6 py-3 text-lg font-semibold text-black bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50 transition duration-150 ease-in-out transform hover:scale-105">
           See the smap!
