fad49e49a8781852b35f68ecb8987e0be15d9e4a
Alexander Haines <alex.haines@artlist.io>
2024-10-25 11:11:39 +0100

    homepage

--- smap/pages/homepage.tsx
+++ smap/pages/homepage.tsx
@@ -1,16 +1,16 @@
-// pages/index.js
-import dynamic from 'next/dynamic';
+import Button from '@mui/material/Button';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const MapWithNoSSR = dynamic(() => import('./mapWrapper'), {
-  ssr: false,
-});
-
-const HomePage = () => {
+export default function Component() {
   return (
-    <div>
-      <MapWithNoSSR />
+    <div className="flex flex-col items-center justify-center min-h-screen bg-teal-500 gap-4">
+      <Image src="/smap.png" alt="smap" width={300} height={400} />
+      <Link href="/map">
+        <Button className="px-6 py-3 text-lg font-semibold text-black bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-opacity-50 transition duration-150 ease-in-out transform hover:scale-105">
+          See the smap!
+        </Button>
+      </Link>
     </div>
   );
-};
-
-export default HomePage;
+}
